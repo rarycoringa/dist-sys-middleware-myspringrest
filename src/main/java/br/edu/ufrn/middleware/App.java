1f7c06013c8acea4f489768c58a5a3fadefee990
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2025-05-21 19:26:05 -0300

    initial version of middleware

---
+++ src/main/java/br/edu/ufrn/middleware/App.java
@@ -0,0 +1,13 @@
+package br.edu.ufrn.middleware;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
